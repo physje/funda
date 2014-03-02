88936655f4aaab66e561bf9761abe9f8cd92d5ca
Matthijs Draijer <git@draijer.org>
2014-03-02 13:58:17 +0100

    Check bij verwijderen huis liep niet goed

--- admin/delete.php
+++ admin/delete.php
@@ -19,22 +19,28 @@ if(isset($_REQUEST['id'])) {
 		$sql_check_unique = "SELECT * FROM $TableResultaat WHERE $ResultaatID like '$id'";
 		$result	= mysql_query($sql_check_unique);
 		
-		if(mysql_num_rows($result) < 1 OR isset($_REQUEST['heelzeker'])) {
+		# Als er maar 1 resultaat is, of als men het zeker weet
+		# mag het huis verwijderd worden
+		if(mysql_num_rows($result) = 1 OR isset($_REQUEST['heelzeker'])) {
+			# Huis zelf verwijderen
 			$sql = "DELETE FROM $TableHuizen WHERE $HuizenID like '$id'";
 			if(mysql_query($sql)) {
 				$deel_1 .= "Huis is verwijderd<br>";
 			}
 			
+			# Kenmerken opschonen			
 			$sql = "DELETE FROM $TableKenmerken WHERE $KenmerkenID like '$id'";
 			if(mysql_query($sql)) {
 				$deel_1 .= "Kenmerken zijn verwijderd<br>";
 			}
 			
+			# Prijzen opschonen
 			$sql = "DELETE FROM $TablePrijzen WHERE $PrijzenID like '$id'";
 			if(mysql_query($sql)) {
 				$deel_1 .= "Huis uit prijzen verwijderd<br>";
 			}
 			
+			# Resultaten opschonen
 			$sql = "DELETE FROM $TableResultaat WHERE $ResultaatID like '$id'";
 			if(mysql_query($sql)) {
 				$deel_1 .= "Huis uit resultaten verwijderd<br>";
@@ -42,6 +48,8 @@ if(isset($_REQUEST['id'])) {
 			
 			$deel_1 .= "<a href='HouseDetails.php'>terug</a><br>";
 		} else {
+			# Mocht het huis in verschillende opdrachten voorkomen,
+			# dan is het de vraag of het wel verstandig is het huis te verwijderen.
 			$deel_1 = "Weet je het echt heel zeker? Dit huis komt namelijk in meerdere opdrachten voor<br>". $sql_check_unique ."<br><a href='?id=$id&zeker=ja&heelzeker=ja'>JA</a> | <a href='HouseDetails.php?id=$id'>NEE</a>";
 		}
 	} else {
