2ee03db255d7bb9a026cac43ee749f4aa188fef1
Matthijs Draijer <git@draijer.org>
2014-08-11 16:35:37 +0200

    Update config.php
    Bugfix (Stond opeens $ScriptMailAdres ipv $ScriptMailAdress)

--- include/config.php
+++ include/config.php
@@ -2,7 +2,7 @@
 
 $ScriptURL		= '';			# Map waar het script staat, bv http://www.example.com/scripts/funda/
 $ScriptTitle		= 'Funda Alert';	# Naam van het script (is naam van afzender in mails)
-$ScriptMailAdres	= '';			# Mailadres van het script (is mailadres van afzender in mails)
+$ScriptMailAdress	= '';			# Mailadres van het script (is mailadres van afzender in mails)
 $Version		= '4.1.1';		# Versie nummer
 $SubjectPrefix		= '[funda] ';		# Voorvoegsel bij de onderwerpregel bij het versturen van mails
 
