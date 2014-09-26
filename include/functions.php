18fec07af06b9c360f38e6539d5039ad6d1a2162
Matthijs Draijer <git@draijer.org>
2014-09-26 18:19:30 +0200

    Update functions.php
    Bug voor open huis opgelost

--- include/functions.php
+++ include/functions.php
@@ -269,7 +269,9 @@ function extractFundaData($HuisText, $verkocht = false) {
 		$voorbehoud = 0;
 	}
 	
-	if(strpos($param[0], '<span class="item-open" title="')) {
+	
+	
+	if(strpos($param[0], '<span class="item-open"') OR strpos($param[0], '<span class="item-open nvm-open-huizen-dag"'))
 		$openhuis = 1;
 	} else {
 		$openhuis = 0;
