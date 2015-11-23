5d855b12bc7ae74dc387f7e0b68c52822448fc5f
Matthijs Draijer <git@draijer.org>
2015-11-23 08:16:40 +0100

    Update check.php

--- check.php
+++ check.php
@@ -3,6 +3,7 @@ include_once('../general_include/general_functions.php');
 include_once('../general_include/general_config.php');
 include_once('../general_include/class.phpmailer.php');
 include_once('../general_include/class.html2text.php');
+include_once('../general_include/class.phpPushover.php');
 include_once('include/functions.php');
 include_once('include/config.php');
 include_once('include/HTML_TopBottom.php');
