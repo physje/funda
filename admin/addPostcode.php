2425e9480360eaec6706b9cd9fc8932fc3facbc5
Matthijs Draijer <git@draijer.org>
2019-08-11 22:17:07 +0200

    Update addPostcode.php
    Om postcodes beter op te kunnen vragen is de database-structuur aangepast.
    In de onderhouds-map 2-scripts runnen (met 09-08-2019 in de bestandsnaam).
    Bij deze push het opvragen van postcode automatisch gerefreshed

--- admin/addPostcode.php
+++ admin/addPostcode.php
@@ -1,6 +1,5 @@
 <?php
 include_once(__DIR__.'/../include/config.php');
-include_once('../include/HTML_TopBottom.php');
 $minUserLevel = 3;
 $cfgProgDir = '../auth/';
 include($cfgProgDir. "secure.php");
@@ -21,20 +20,39 @@ $nummer 		= $data['nummer'];
 $letter			= $data['letter'];
 $toevoeging	= $data['toevoeging'];
 
-$Rechts[] = '<b>Details</b>';
-$Rechts[] = 'ID@funda : '. $row[$HuizenID];
-$Rechts[] = 'Volledige adres : '.$data['adres'];
-$Rechts[] = 'Straatnaam :'. $straat;
-$Rechts[] = 'Huisnummer : '. $nummer;
-$Rechts[] = 'Huisletter : '. $letter;
-$Rechts[] = 'Huisnummertoevoeging : '. $toevoeging;
-$Rechts[] = 'Woonplaats : '. $data['plaats'];
+$Rechts[] = '<table>';
+$Rechts[] = '<tr>';
+$Rechts[] = '	<td colspan=2><b>Details</b></td>';
+$Rechts[] = '</tr>';
+$Rechts[] = '<tr>';
+$Rechts[] = '	<td>ID@funda</td><td>'. $row[$HuizenID] .'</td>';
+$Rechts[] = '</tr>';
+$Rechts[] = '<tr>';
+$Rechts[] = '	<td>Volledige adres</td><td>'.$data['adres'] .'</td>';
+$Rechts[] = '</tr>';
+$Rechts[] = '<tr>';
+$Rechts[] = '	<td>Straatnaam</td><td>'. $straat .'</td>';
+$Rechts[] = '</tr>';
+$Rechts[] = '<tr>';
+$Rechts[] = '	<td>Huisnummer</td><td>'. $nummer .'</td>';
+$Rechts[] = '</tr>';
+$Rechts[] = '<tr>';
+$Rechts[] = '	<td>Huisletter</td><td>'. $letter .'</td>';
+$Rechts[] = '</tr>';
+$Rechts[] = '<tr>';
+$Rechts[] = '	<td>Huisnummertoevoeging</td><td>'. $toevoeging .'</td>';
+$Rechts[] = '</tr>';
+$Rechts[] = '<tr>';
+$Rechts[] = '	<td>Woonplaats : '. $data['plaats'] .'</td>';
+$Rechts[] = '</tr>';
+$Rechts[] = '</table>';
 
 if(is_numeric($nummer)) {
     $PC = findPCbyAdress($straat, $nummer, $letter, $toevoeging, $data['plaats']);
 
     if(updatePC($row[$HuizenID], $PC)) {
         $Links[] = 'Gelukt : '. $PC;
+        $userInteraction = false;
     } else {
         $Links[] = 'Geen postcode kunnen updaten';
     }
@@ -42,9 +60,10 @@ if(is_numeric($nummer)) {
     $Links[] = 'Controleer input parameters, het huisnummer is niet numeriek : '. $nummer;
 }
 
+include_once('../include/HTML_TopBottom.php');
 echo $HTMLHeader;
 echo "<tr>\n";
 echo "<td width='50%' valign='top' align='center'>". showBlock(implode("\n", $Links)) ."</td>";
-echo "<td width='50%' valign='top' align='center'>". showBlock(implode("<br>\n", $Rechts)) ."</td>";
+echo "<td width='50%' valign='top' align='center'>". showBlock(implode("\n", $Rechts)) ."</td>";
 echo "</tr>\n";
 echo $HTMLFooter;
