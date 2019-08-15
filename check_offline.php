94890b5d123e13fcc08d37aabe20fac3a7562fb5
Matthijs Draijer <git@draijer.org>
2019-08-15 15:23:08 +0200

    Update check_offline.php
    Andere manier van matchen aan zoekopdracht of detailpagina bij offline HTML-funda-pagina

--- check_offline.php
+++ check_offline.php
@@ -2,13 +2,14 @@
 include_once(__DIR__.'/include/config.php');
 include_once(__DIR__ .'/include/HTML_TopBottom.php');
 include_once($cfgGeneralIncludeDirectory.'class.phpPushover.php');
+$db = connect_db();
 $minUserLevel = 3;
 $cfgProgDir = 'auth/';
 include($cfgProgDir. "secure.php");
 
-$db = connect_db();
 
 $String = $block = array();
+$success = false;
 	
 if ($handle = opendir($offlineDir)) {
 	while (false !== ($entry = readdir($handle))) {
@@ -29,7 +30,7 @@ foreach($files as $file) {
 	set_time_limit (30);	
 	$String = $AdressenArray = array();
 	$OpdrachtID = 0;
-	$detail = $verkocht = $overzicht = false;
+	$succes = $detail = $verkocht = $overzicht = false;
 	
 	$bestand = $offlineDir.$file;
 		
@@ -41,38 +42,76 @@ foreach($files as $file) {
 	# 	Overzichtspagina met te koop staande huizen
 	# 	Overzichtspagina met verkochte huizen
 	# 	Detail pagina van een individueel huis	
+	
+	# in de HTML-code staat altijd de bezochte URL
+	# Die moeten wij zien te vinden
 	$appHeaderLink	= getString('appheader-English-link" class="app-header__link" href="', '" hreflang=', $contents, 0);
-	$pageURL				= getString('/koop/', '', $appHeaderLink[0], 0);	
-	$zoekURL				= '/koop/'.$pageURL[0];
+	$pageURL		= getString('/koop/', '', $appHeaderLink[0], 0);	
+	$zoekURL		= '/koop/'.$pageURL[0];
+	
+	# Aantal filters in de URL hebben 'geen' waarde en mogen er dus uit
 	$cleanZoekString = str_replace('/verkocht/', '/', $zoekURL);
-		
+	$cleanZoekString = str_replace('/sorteer-afmelddatum-af/', '/', $cleanZoekString);
+
+    # Door hem op te knippen zien wij welke filters er actief zijn
+	$filtersZoeken = explode('/', $cleanZoekString);
+	
+	# Als in de zoekURL de tekst /verkocht/ voorkomt gaat het over een huis wat verkocht is
+	# De variabele $verkocht is dan waar
+	if(strpos($zoekURL, '/verkocht/')) {
+		$verkocht		= true;
+	} else {
+	    $verkocht		= false;
+	}
+	
+	# Doorloop alle opdrachten
 	foreach($opdrachten as $opdracht) {
 		$opdrachtData = getOpdrachtData($opdracht);
 		$cleanOpdrachtURL = str_replace('http://www.funda.nl', '', $opdrachtData['url']);
+		$filtersOpdracht = explode('/', $cleanOpdrachtURL);
 		
-		# Zoeken naar tekoopstaande huizen		
-		# /p2/ en /p3/ etc. moet er van worden afgeknipt dus even zo
-		if(substr($zoekURL, 0, strlen($cleanOpdrachtURL)) == $cleanOpdrachtURL){
-			$OpdrachtID = $opdracht;
-			$overzicht = true;
-			$verkocht = false;
+		# Wij gaan gevonden filters verwijderen dus maken even een kopie van het orgineel
+		$kopieFiltersZoeken = $filtersZoeken;
+		
+		# Wij lopen 2x de filters door
+		# 1x kijken wij welke filters uit de HTML-pagina voorkomen in de zoekopdracht
+		# 1x kijken wij welke filters uit de zoekopdracht voorkomen in de HTML-pagin
+		# Die combi die op beide het beste scoort is met redelijke zekerheid de zoekopdracht
+		foreach($filtersOpdracht as $key => $value) {
+		    if(in_array($value, $kopieFiltersZoeken)) {
+		        $index = array_search ($value, $kopieFiltersZoeken);
+		        unset($kopieFiltersZoeken[$index]);
+		    }
 		}
 		
-		# Zoeken naar verkochte huizen
-		if(substr($cleanZoekString, 0, strlen($cleanOpdrachtURL)) == $cleanOpdrachtURL AND strpos($zoekURL, '/verkocht/')){
-			$OpdrachtID = $opdracht;
-			$overzicht = true;
-			$verkocht = true;
-		}		
+        foreach($filtersZoeken as $key => $value) {
+		    if(in_array($value, $filtersOpdracht)) {
+		        $index = array_search ($value, $filtersOpdracht);
+		        unset($filtersOpdracht[$index]);
+		    }
+		}
+		
+		//echo $opdracht .' -> '. count($kopieFiltersZoeken) .'|'. count($filtersOpdracht) .' = '. (count($kopieFiltersZoeken)+count($filtersOpdracht)) .'<br>';
+		//echo implode('|', $kopieFiltersZoeken) .' niet gevonden<br>';
+		//echo '<br>';
+		
+		$score_zoek[$opdracht] = count($kopieFiltersZoeken);
+		$score_opdracht[$opdracht] = count($filtersOpdracht);
+		$score_tot[$opdracht] = count($kopieFiltersZoeken)+count($filtersOpdracht);
 	}
+	
+	# Als de laagste totale score 0 of 1 is, is het aannemelijk dat we een match hebben
+	if(min($score_tot) < 2) {
+	    $OpdrachtID = array_search (min($score_tot), $score_tot);
+		$overzicht = true;
+	} else {
+	    $overzicht = false;
+	}
+
 	
 	# Het lijkt geen overzichtspagina te zijn
 	# Onderzoek de optie detailpagina
 	if(!$overzicht) {
-		if(strpos($zoekURL, '/verkocht/')) {
-			$verkocht		= true;
-		}
-		
 		$mappen = explode("/", $zoekURL);
 		 
 		if($verkocht) {
@@ -83,7 +122,7 @@ foreach($files as $file) {
 		
 		$fundaID	= $delen[1];
 		
-		if(is_numeric($fundaID)) {
+		if(is_numeric($fundaID) AND count($mappen) > 4 AND count($delen) > 3) {
 			$detail = true;
 		}
 	}	
@@ -158,6 +197,7 @@ foreach($files as $file) {
 				if(!saveHouse($data, $extraData)) {
 					$ErrorMessage[] = "Toevoegen van ". $data['adres'] ." aan het script ging niet goed";
 					toLog('error', $OpdrachtID, $data['id'], 'Huis toevoegen aan script mislukt');
+					$success = false;
 				} else {					
 					toLog('info', $OpdrachtID, $data['id'], 'Huis toevoegen aan script');
 				}
@@ -290,6 +330,7 @@ foreach($files as $file) {
 		}
 	
 		$String[] = "<a href='$bestand'>Overzicht</a>". ($verkocht ? ' met verkochte huizen ' : ' ')."van <a href='$OpdrachtURL'>". $OpdrachtData['naam'] ."</a>; ". count($AdressenArray)  ." huizen gevonden<br>";
+		$success = true;
 			
 		toLog('debug', $OpdrachtID, '', "Einde pagina (". count($AdressenArray) ." huizen)");		
 	
@@ -305,12 +346,14 @@ foreach($files as $file) {
 		
 		if($fundaID != $data['id']) {
 			$String[] = "Klopt dit wel ?";
+			$success = false;
 		} else {
 			$String[] = "Details van <a href='". $ScriptURL ."admin/edit.php?id=$fundaID'>". $data['adres'] ."</a> ingelezen<br>\n";
 			
 			# Als wij een huis niet kennen klopt er iets niet
 			if(!knownHouse($fundaID)) {
 				toLog('error', '', $fundaID, 'Huis niet bekend');
+				$success = false;
 					
 			# Meestal zal het huis wel bekend zijn
 			} else {
@@ -331,14 +374,13 @@ foreach($files as $file) {
 				
 				toLog('info', '', $fundaID, 'Offline pagina ingeladen');
 				remove4Details($fundaID);
+				$success = true;
 			}
 		}
 	}
 	
-	# Alleen als het een overzichts-pagina of detail-pagina is
-	#		(lees : de data hierboven is geïmporteerd)
-	# de pagina verwijderen
-	if($overzicht OR $detail) {
+	# Alleen als de import succesvol is verlopen mag de pagina verwijderd worden
+	if($success) {
 		unlink($bestand);
 	}
 	
