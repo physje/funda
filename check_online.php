56d8411de2cd21062ebf33ed6941eb37acf14a0a
Matthijs Draijer <git@draijer.org>
2020-03-28 09:31:24 +0100

    bugfix

--- check_online.php
+++ check_online.php
@@ -1,6 +1,6 @@
 <?php
-include_once(__DIR__.'/../include/config.php');
-include_once(__DIR__.'/../include/HTML_TopBottom.php');
+include_once(__DIR__.'/include/config.php');
+include_once(__DIR__.'/include/HTML_TopBottom.php');
 
 $db = connect_db();
 
